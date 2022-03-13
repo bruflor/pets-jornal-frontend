63e3b90d9b1c158fadb0bd188b6c67b66ed39d0c
Bruna Flôr <brn.flor@gmail.com>
2022-03-13 22:02:08 +0000

    wip axios

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,13 @@
+import axios from 'axios'
 import { Cards } from "../../components/Cards"
 // import { NewCard } from "../../components/Cards/newCard"
 import { useParams } from "react-router-dom"
 import "./style.css"
-import myDataBase from "../../database/pets.json"
+// import myDataBase from "../../database/pets.json"
+
+const petsDoAxios = axios.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados/33/distritos').then(function(response){
+    console.log(response.data)
+})
 
 
 const Dashboard = () => {
@@ -10,15 +15,15 @@ const Dashboard = () => {
     //user_id is coming from useParams
     const {user_id} = useParams()
     
-    const userIndex = myDataBase.findIndex(user => user.id === user_id)
+    // const userIndex = myDataBase.findIndex(user => user.id === user_id)
 
-    const myUser = myDataBase[userIndex]
+    // const myUser = myDataBase[userIndex]
 
     return (
 
         <div className="dashboard">
 
-            <h1>{myUser.name} Dashboard</h1>
+            {/* <h1>{myUser.name} Dashboard</h1> */}
 
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet iusto maiores, dolor iste doloribus ad dolore. Modi quo, commodi hic quibusdam dolorem libero dolores quod ducimus cupiditate, excepturi aut dolor!</p>
 
@@ -28,11 +33,11 @@ const Dashboard = () => {
 
                 <div className="cardsLine">
 
-                    {myUser.pets.map(pet =>
+                    {/* {myUser.pets.map(pet =>
 
                         <Cards userId={user_id}name={pet.name} weight={pet.weight} key={pet.id} petId={pet.id}></Cards>
 
-                    )}
+                    )} */}
 
                     {/* <NewCard /> */}
 
